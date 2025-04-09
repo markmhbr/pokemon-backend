e9bda33794c12b895e803f7f3aed093b268c0eaa
fajarajay1 <bna671674>
2025-04-09 13:29:10 +0700

    pakai environment variable untuk MongoDB URI

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,18 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { HttpModule } from '@nestjs/axios';
-
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://miftaalfareza:markmhbr4525@cluster0.rdi2n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
+    ConfigModule.forRoot({
+      isGlobal: true, // biar bisa dipake di semua module
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
     HttpModule,
     PokemonModule,
   ],
 })
 export class AppModule {}
+
 
