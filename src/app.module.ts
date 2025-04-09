72524c9f40fd973c3db2ce06012d4e3ae294b8e4
fajarajay1 <bna671674>
2025-04-09 13:51:32 +0700

    uhuy

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true, // biar bisa dipake di semua module
     }),
-    MongooseModule.forRoot(process.env.MONGODB_URI as string),
+    MongooseModule.forRoot(process.env.MONGODB_URI + '&tls=true'),
     HttpModule,
     PokemonModule,
   ],
